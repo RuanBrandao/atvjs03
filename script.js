591adea6ce09d39990aa8dc8dbb9b724f74a0474
RUAN LUIS MATOS BRANDAO SUAREZ <200026832@ucsal.br>
2023-09-06 09:14:05 -0300

    Adicionando Arquivos

---
+++ script.js
@@ -0,0 +1,12 @@
+var n = prompt("Insira um número")
+
+if(n>0) {
+    alert("Este número é positivo!")
+} else if (n==0) {
+    alert("Este número é zero!")
+} else if (n<0){
+    alert("Este número é negativo!")
+} else {
+    alert("Isto não é um número!")
+}
+
